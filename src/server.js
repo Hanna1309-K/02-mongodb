99dcc9491fb8933fc073a9adb95277b27bffe0d7
anna <anytagolenok2@ukr.net>
2026-05-15 20:55:06 +0200

    02-mongodb homework

---
+++ src/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import notesRoutes from './routes/notesRoutes.js';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+dotenv.config();
+
+const app = express();
+
+const startServer = async () => {
+    // 1. Підключення до MongoDB ПЕРЕД запуском сервера
+    await connectMongoDB();
+
+    // 2. Middleware
+    app.use(logger);
+    app.use(cors());
+    app.use(express.json());
+
+    // 3. Роути
+    app.use('/notes', notesRoutes);
+
+    // 4. 404 middleware (після всіх роутів)
+    app.use(notFoundHandler);
+
+    // 5. error handler (останній)
+    app.use(errorHandler);
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+};
+
+startServer();
