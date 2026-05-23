d592a3d3a1404914eb1f2f2ba333dd83c266042d
anna <anytagolenok2@ukr.net>
2026-05-23 18:25:07 +0200

    Complete validation, filtering and pagination for notes API

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
+
 import {
     getAllNotes,
     getNoteById,
@@ -7,11 +9,48 @@ import {
     updateNote,
 } from '../controllers/notesController.js';
 
+import {
+    getAllNotesSchema,
+    noteIdSchema,
+    createNoteSchema,
+    updateNoteSchema,
+} from '../validations/notesValidation.js';
+
 const router = Router();
-router.get('/notes', getAllNotes);
-router.get('/notes/:noteId', getNoteById);
-router.post('/notes', createNote);
-router.delete('/notes/:noteId', deleteNote);
-router.patch('/notes/:noteId', updateNote);
+
+// GET /notes
+router.get(
+    '/notes',
+    celebrate(getAllNotesSchema),
+    getAllNotes
+);
+
+// GET /notes/:noteId
+router.get(
+    '/notes/:noteId',
+    celebrate(noteIdSchema),
+    getNoteById
+);
+
+// POST /notes
+router.post(
+    '/notes',
+    celebrate(createNoteSchema),
+    createNote
+);
+
+// DELETE /notes/:noteId
+router.delete(
+    '/notes/:noteId',
+    celebrate(noteIdSchema),
+    deleteNote
+);
+
+// PATCH /notes/:noteId
+router.patch(
+    '/notes/:noteId',
+    celebrate(updateNoteSchema),
+    updateNote
+);
 
 export default router;
